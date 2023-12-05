25f9db4542f453fcf9847172dafc4eab03739046
keith27staines <keith27staines@icloud.com>
2023-12-05 08:08:24 +0000

    Added insertAbove

--- Sources/DiscoveryTreeCore/Tree/Tree.swift
+++ Sources/DiscoveryTreeCore/Tree/Tree.swift
@@ -35,7 +35,7 @@ public class Tree<Content: Codable>: Codable {
     /// Inserts a new node above the receiver
     ///
     /// * The receiver is appended to the new node's  children
-    /// * If the receiver is already has a parent then the receiver is removed from the parent' and the new node is inserted  at the same child index
+    /// * If the receiver already has a parent then the receiver is removed from the parent and the new node is inserted  at the same child index
     /// - Returns: The new node
     /// - Throws: If there is an unexpected error
     public func insertNewTreeAbove() throws -> Tree {
@@ -50,6 +50,18 @@ public class Tree<Content: Codable>: Codable {
         try _ = parent.replaceChild(at: childIndex, with: new)
         try new.appendChild(self)
         return new
+    }
+    
+    public func insertAbove(_ node: Tree<Content>) throws {
+        guard let parent = parent else {
+            try node.appendChild(self)
+            return
+        }
+        guard let childIndex = self.childIndex() else {
+            throw TreeError.unexpectedError
+        }
+        try _ = parent.replaceChild(at: childIndex, with: node)
+        try node.appendChild(self)
     }
     
     /// Add a child tree to the current instance
